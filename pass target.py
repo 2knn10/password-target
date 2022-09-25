3572785921b1fdde7c864f2ff4532ddb8f0b1f8a
2knn10 <109735963+2knn10@users.noreply.github.com>
2022-09-25 16:45:30 +0300

    Create pass target.py

---
+++ pass target.py
@@ -0,0 +1,188 @@
+import random
+style = '''\033[31m
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⠙⠻⢶⣄⡀⠀⠀⠀⢀⣤⠶⠛⠛⡇⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣇⠀⠀⣙⣿⣦⣤⣴⣿⣁⠀⠀⣸⠇⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣡⣾⣿⣿⣿⣿⣿⣿⣿⣷⣌⠋⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⣷⣄⡈⢻⣿⡟⢁⣠⣾⣿⣦⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⠘⣿⠃⣿⣿⣿⣿⡏⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠈⠛⣰⠿⣆⠛⠁⠀⡀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣦⠀⠘⠛⠋⠀⣴⣿⠁⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣾⣿⣿⣿⣿⡇⠀⠀⠀⢸⣿⣏⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣠⣶⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠀⠀⠀⠾⢿⣿⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣠⣿⣿⣿⣿⣿⣿⡿⠟⠋⣁⣠⣤⣤⡶⠶⠶⣤⣄⠈⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢰⣿⣿⣮⣉⣉⣉⣤⣴⣶⣿⣿⣋⡥⠄⠀⠀⠀⠀⠉⢻⣄⠀⠀⠀⠀⠀
+⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣋⣁⣤⣀⣀⣤⣤⣤⣤⣄⣿⡄⠀⠀⠀⠀
+⠀⠀⠀⠀⠙⠿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⠋⠉⠁⠀⠀⠀⠀⠈⠛⠃⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+ 
+|=================================================|
+|      [ + ] by kavani                            |
+|=================================================|
+|      [ + ] Instagram : 2knn01                   |                   
+|=================================================|
+|      [ + ] Telegram : https://t.me/kavani2knn10 |                   
+|=================================================| 
+'''
+print(style)
+
+
+uuser = name = input('Enter targer Username @  : ')
+barth = birth = input('Date of Year of Birth /0000 : ')
+llist = listname = input('List Name : ')
+ffirst = firstname = input('First Name : ')
+phone =  input('Number Phone : ')
+inss = ins = input('Instagram User : ')
+A = ""+name
+b = ""+birth
+l = ""+listname
+f = ""+firstname
+n = "123456789"
+i = ""+ins
+o = "12"
+p = "21"
+w = "123456789"
+all = A + b + l + f + n + i + o + p + w
+length = 8
+name ="".join(random.sample(all,length))
+birth ="".join(random.sample(all,length))
+listname ="".join(random.sample(all,length))
+firstname ="".join(random.sample(all,length))
+n ="".join(random.sample(all,length))
+ins ="".join(random.sample(all,length))
+q ="".join(random.sample(all,length))
+w ="".join(random.sample(all,length))
+print('|=================================================| ')
+print(""+uuser+barth)
+print(""+uuser+barth+o)     
+print(""+uuser+barth+p)                         
+print(""+barth+uuser) 
+
+print(""+barth+uuser+o)
+print(""+barth+uuser+p)                         
+print(""+name+n)     
+
+print(""+uuser+llist)   
+print(""+uuser+llist+o)
+print(""+uuser+llist+p) 
+
+print(""+barth+uuser)    
+print(""+barth+uuser+o)
+print(""+barth+uuser+p) 
+
+print(""+name+birth)  
+print(""+name+birth+o)
+print(""+name+birth+p) 
+
+print(""+birth+name)   
+print(""+birth+name+o)
+print(""+birth+name+p)
+
+print(""+llist+uuser)     
+print(""+llist+uuser+o)
+print(""+llist+uuser+p) 
+
+print(""+uuser+llist)  
+print(""+uuser+llist+o)
+print(""+uuser+llist+p) 
+
+print(""+name+listname)     
+print(""+name+listname+o)
+print(""+name+listname+p) 
+
+print(""+listname+name)
+print(""+listname+name+o)
+print(""+listname+name+p) 
+
+print(""+listname+barth) 
+print(""+listname+barth+o)
+print(""+listname+barth+p) 
+
+print(""+barth+listname)   
+print(""+barth+listname+o)
+print(""+barth+listname+p) 
+
+print(""+ffirst+uuser) 
+print(""+ffirst+uuser+o)
+print(""+ffirst+uuser+p)
+
+print(""+ffirst+barth)   
+print(""+ffirst+barth+o)
+print(""+ffirst+barth+p) 
+
+print(""+ffirst+llist)    
+print(""+ffirst+llist+o)
+print(""+ffirst+llist+p)
+
+print(""+firstname+name)  
+print(""+firstname+name+o)
+print(""+firstname+name+p) 
+
+print(""+firstname+birth)    
+print(""+firstname+birth+o)
+print(""+firstname+birth+p) 
+
+print(""+firstname+listname)    
+print(""+firstname+listname+o)
+print(""+firstname+listname+p)  
+
+print(''+phone)  
+print(''+phone+o)
+print(''+phone+p)  
+
+print(''+ins)        
+print(''+ins+o)
+print(''+ins+p)  
+
+print("_2000"+uuser)
+print("_2001"+uuser)
+print("_2002"+uuser)
+
+print("_2003"+uuser)
+print("_2004"+uuser)
+print("_2005"+uuser)
+
+print("_2006"+uuser)
+print("_2007"+uuser)
+print("_2008"+uuser)
+
+print("_2009"+uuser)
+print("_2010"+uuser)  
+
+print("123456789")    
+print("12345678")
+print("12345679")
+
+print("12345689")
+print("12345789")
+print("12346789")
+
+print("12356789")
+print("12456789")
+print("13456789")
+
+print("23456789")
+print("987654321")
+print("87654321")
+
+print("97654321")
+print("98654321")
+print("98754321")
+
+print("98764321")
+print("98765321")
+print("98765421")
+
+print("98765431")
+print(""+w)
+print('|=================================================| ')
+
+
+
+
+
+
+
+
+
+
